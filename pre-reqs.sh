deb015d779e5a3c3e15ef168f4341976361ebd8c
abondili51 <aravind.singh2006@gmail.com>
2021-04-03 15:07:40 -0400

    Update pre-reqs.sh

--- pre-reqs.sh
+++ pre-reqs.sh
@@ -14,5 +14,5 @@ firewall-cmd --permanent --add-port=80/tcp
 firewall-cmd --permanent --add-port=8850/tcp
 systemctl restart firewalld
 
-echo "Donwloading config files..."
-git clone https://github.com/abondili51/TS-Auto.git /tmp/ts-auto
+#echo "Donwloading config files..."
+#git clone https://github.com/abondili51/TS-Auto.git /tmp/ts-auto
