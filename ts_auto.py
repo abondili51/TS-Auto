4b058ccbb04c439eae140bc557d29712290a687d
abondili51 <aravind.singh2006@gmail.com>
2021-04-06 17:29:33 -0400

    Update ts_auto.py

--- ts_auto.py
+++ ts_auto.py
@@ -36,24 +36,24 @@ def activate_register():
     license_cmd='tsm licenses activate -t'
     run_os_cmd(license_cmd,'Activating Trial License...')
 
-    register_cmd='tsm register --file /tmp/ts-auto/config/register.json'
+    register_cmd='/opt/tableau/tableau_server/packages/customer-bin.20211.21.0320.1853/tsm register --file /tmp/ts-auto/config/register.json'
     run_os_cmd(register_cmd,'Registratering...')
 
 def configure():
-    id_store_cmd='tsm settings import -f /tmp/ts-auto/config/id_store.json'
+    id_store_cmd='sudo /opt/tableau/tableau_server/packages/customer-bin.20211.21.0320.1853/tsm settings import -f /tmp/ts-auto/config/id_store.json'
     run_os_cmd(id_store_cmd,'Configuring ID store settings...')
 
-    apply_changes='tsm pending-changes apply'
+    apply_changes='sudo /opt/tableau/tableau_server/packages/customer-bin.20211.21.0320.1853/tsm pending-changes apply'
     run_os_cmd(apply_changes,'Applying pending changes...')
 
 def start_ts():
-    start_ts='tsm initialize --start-server --request-timeout 1800'
+    start_ts='sudo /opt/tableau/tableau_server/packages/customer-bin.20211.21.0320.1853/tsm initialize --start-server --request-timeout 1800'
     run_os_cmd(start_ts,'Initializing and Starting Tableau Server...')
 
 def add_admin(server_ip):
     pwd=base64.b64decode('YWRtaW4=').decode()
     server='http://'+server_ip
-    add_admin=f"tabcmd initialuser --server {server} --username 'admin' --password {pwd}"
+    add_admin=f"sudo /opt/tableau/tableau_server/packages/customer-bin.20211.21.0320.1853/tabcmd initialuser --server {server} --username 'admin' --password {pwd}"
     run_os_cmd(add_admin,'Adding admin account...')
     print('Admin account is successfully added!')
 
